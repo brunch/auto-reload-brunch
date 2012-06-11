2a2645161180158bc0c0cde4c88f716ecdc758ba
ilia choly <ilia.choly@gmail.com>
2012-06-10 23:43:54 -0400

    only reload css links when receiving 'stylesheet' message

--- vendor/auto-reload.js
+++ vendor/auto-reload.js
@@ -3,11 +3,33 @@
   var br = window.brunch;
   if (!WebSocket || !br || !br['auto-reload'] || !br['auto-reload'].enabled) return;
 
+  var cacheBuster = function(url){
+      var date = Math.round(+new Date()/1000).toString()
+      url = url.replace(/(\&|\\?)cacheBuster=\d*/,'');
+      return url + ( url.indexOf('?') >=0 ? '&':'?') +'cacheBuster=' + date;
+  }
+
+  var reloaders = {
+    page: function(){
+      window.location.reload(true);
+    },
+    stylesheet: function(){
+      var links = document.getElementsByTagName('link');
+      for(var i=0; i<links.length; i++) {
+        var link = links[i];
+        if(link.href != null && link.rel.toLowerCase().indexOf('stylesheet') != -1) {
+          link.href = cacheBuster(link.href); 
+        }
+      }
+    }
+  }
+
   var connection = new WebSocket('ws://' + window.location.hostname + ':9485');
   connection.onmessage = function(event) {
     var message = event.data;
     var b = window.brunch;
-    var reloadEnabled = !!(b && b['auto-reload'] && b['auto-reload'].enabled);
-    if (message === 'compile' && reloadEnabled) window.location.reload();
+    if(!b || !b['auto-reload'] || !b['auto-reload'].enabled) return;
+    if(reloaders[message] != null){ reloaders[message](); }
+    else{ reloaders.page(); }
   };
 })();
